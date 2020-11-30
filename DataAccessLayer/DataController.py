6d1ddd80f7c1f90ac323a7f7f07fdf07bc09e269
majdsh1 <73431721+majdsh1@users.noreply.github.com>
2020-11-30 23:49:51 +0200

    Add files via upload

---
+++ DataAccessLayer/DataController.py
@@ -0,0 +1,37 @@
+from DataAccessLayer.Object.User import User
+#import firebase
+class DataController:
+
+    def __init__(self):
+        # connecting to the database
+        # from firebase import firebase
+        #firebase = firebase.FirebaseApplication('https://covid19edu-d1f73.firebaseio.com/','user')
+       pass
+
+    def Add(self, user):
+          #add the database
+          #result = firebase.post('https://covid19edu-d1f73.firebaseio.com/', data)
+        pass
+
+
+
+
+    def Update(self, user):
+        # updating the database
+        # from firebase import firebase
+
+       # firebase = firebase.FirebaseApplication('https://covid19edu-d1f73.firebaseio.com/', None)
+       # firebase.put('/python-example-f6d0b/Students/-LjLUhaWGuxNd5gOEmse', 'Name', 'Bob')
+        #print('Record Updated')
+        pass
+
+    def delete(self, user):
+        #deleting the data from the database
+       # from firebase import firebase
+       # firebase = firebase.FirebaseApplication('https://covid19edu-d1f73.firebaseio.com', None)
+       # firebase.delete(self.user)
+        pass
+
+    def getData(self):
+        #getting the data from the database
+        pass
